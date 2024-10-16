d925731f0e5a9fa7d0181d814a50d14b6d8f6b64
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2024-10-16 20:20:16 -0300

    ft_strlcat removing redundancy

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:45:41 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/10/16 20:07:23 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/10/16 20:19:23 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	src_size = ft_strlen(src);
 	if (size <= dst_size)
 		return (size + src_size);
-	if (size == 0)
-		return (src_size);
 	size = size - dst_size;
 	dst += dst_size;
 	while (size-- > 1 && *src)
