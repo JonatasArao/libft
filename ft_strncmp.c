860c507c143974d82e48e4275c28de63eadcbaae
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-10-21 11:56:18 -0300

    ft_strncmp improved

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:46:55 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/10/17 17:06:07 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/10/21 11:47:27 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n-- > 0 && (*s1 || *s2))
+	if (n == 0)
+		return (0);
+	while (n-- > 1 && *s1 == *s2 && (*s1 || *s2))
 	{
-		if (*s1 != *s2)
-			return ((unsigned char) *s1 - (unsigned char) *s2);
 		s1++;
 		s2++;
 	}
-	return (0);
+	return ((unsigned char) *s1 - (unsigned char) *s2);
 }
