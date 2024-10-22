7932f943c2a6d62a60216f0fdf3840d4ac18fbc9
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2024-10-22 09:34:18 -0300

    ft_putendl_fd added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 08:50:14 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/10/22 09:02:34 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/10/22 09:15:31 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,4 +65,5 @@ void	ft_putchar_fd(char c, int fd);
 
 void	ft_putstr_fd(char *s, int fd);
 
+void	ft_putendl_fd(char *s, int fd);
 #endif
