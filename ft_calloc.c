d7b633360f93991bed144579dfc2cfa394e4c54e
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2024-10-28 11:34:27 -0300

    ft_calloc preventing integer overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,25 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 15:12:26 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/10/19 19:38:59 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/10/28 11:32:57 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_check_multiplication_overflow(size_t x, size_t y)
+{
+	if (x && y && (x * y) / x != y && (x * y) / y != x)
+		return (1);
+	return (0);
+}
+
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+	if (ft_check_multiplication_overflow(nmemb, size))
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
