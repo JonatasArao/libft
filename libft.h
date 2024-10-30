2e1c48464967c4408be4d782f3c46e4b6753c375
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2024-10-30 14:42:58 -0300

    update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 08:50:14 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/10/30 14:41:56 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/10/30 14:42:16 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,6 +107,6 @@ void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
